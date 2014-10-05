8695a83d47f1f4c4d4d6b7ba379e8f89f5eae785
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2014-10-05 14:07:37 +0300

    Added some example mysql db stuff

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,41 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scala.slick.driver.MySQLDriver.simple._
+import models._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready!"))
-  }
+	def index = Action {
+	    
+	    
+	    val driver = Play.current.configuration.getString("db.default.driver").getOrElse("")
+	    val url = Play.current.configuration.getString("db.default.url").getOrElse("")
+	    val db = Database.forURL(url, driver = "com.mysql.jdbc.Driver")
+	    
+	    /*
+	     // the code below creates the tables and inserts some example code
+		val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
+
+		// the query interface for the Coffees table
+        val coffees: TableQuery[Coffees] = TableQuery[Coffees]
+		
+		// find a way to get the settings from application.conf instead
+		val db = Database.forURL("jdbc:mysql://localhost/slicktest?user=root&characterEncoding=UTF-8", driver = "com.mysql.jdbc.Driver")
+		
+		
+		db.withSession { implicit session =>
+			// commented out, this creates the both tables, can be run only once
+			//(suppliers.ddl ++ coffees.ddl).create
+		    suppliers += (101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199")
+		    suppliers += ( 49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460")
+		    suppliers += (150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966")
+		}
+		*/
+	    
+	    
+	    
+		Ok(views.html.index("Your new application is ready! " + driver))
+	}
 
 }
