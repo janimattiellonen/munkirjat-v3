2d4dc291c453510e0374c8527036fbd4c781dbb6
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2014-10-26 23:06:31 +0200

    Initial version of StatSidebar

--- public/core/javascripts/munkirjat/app.js
+++ public/core/javascripts/munkirjat/app.js
@@ -1,4 +1,5 @@
 var app = angular.module('app', [
+   'ngRoute',                                 
    'ngResource',
    'ui.router'                   
 ]);
@@ -13,4 +14,22 @@ app.config(['$stateProvider', '$urlRouterProvider',
         		url: '/about',
                 templateUrl: '/templates/about'
             });
+}]);
+
+
+app.directive('statsSidebar', ['$compile', function($compile) {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div class="stats"><h2>Statistics</h2><div class="item" ng-repeat="i in items"><h3>{{ i.title }}</h3><p>{{ i.value }}</p></div></div>',
+        controller: function($scope, $element, $attrs, $location) {
+
+        	var items = [
+        	     {title: "Foo", value: "Bar"},
+        	     {title: "Foo2", value: "Bar2"}
+        	];
+
+        	$scope.items = items;
+        }
+    };
 }]);
